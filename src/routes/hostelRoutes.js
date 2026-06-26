71f04c3e5a30b83dd18681e2aadc6683b99afe3a
Cutha <danielcutha930@gmail.com>
2026-06-26 14:41:33 +0300

    Fix import route order

--- src/routes/hostelRoutes.js
+++ src/routes/hostelRoutes.js
@@ -1,3 +1,4 @@
+const { importFromOpenStreetMap } = require('../controllers/importController');
 const express = require('express');
 const router = express.Router();
 const upload = require('../middleware/upload');
@@ -13,11 +14,14 @@ const {
 
 // Public
 router.get('/nearby', getNearbyHostels);
-router.get('/:id', getHostelById);
 
-// Owner only
+// Owner only (must be before /:id)
+router.post('/import/osm', authenticate, requireOwner, importFromOpenStreetMap);
 router.post('/', authenticate, requireOwner, upload.array('photos', 10), createHostel);
 router.get('/mine/list', authenticate, requireOwner, getMyHostels);
+
+// Dynamic routes (must be last)
+router.get('/:id', getHostelById);
 router.put('/:id', authenticate, requireOwner, updateHostel);
 router.delete('/:id', authenticate, requireOwner, deleteHostel);
 
